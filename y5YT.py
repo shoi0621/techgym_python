769676066992eb7b3184ef6afe9e0d46ff7208dd
shoi0621 <94964207+shoi0621@users.noreply.github.com>
2021-11-24 19:30:52 +0900

    Update y5YT.py

--- y5YT.py
+++ y5YT.py
@@ -6,13 +6,13 @@ print('自分の手を入力してください')
 my_hand = int(input('0:グー, 1:チョキ, 2:パー'))
 you_hand = random.randint(0, 2)
 
-if my_hand == 0:
-  if you_hand == 0:
+if my_hand == 2:
+  if you_hand == 2:
     print('あいこ')
-  elif you_hand == 1:
+  elif you_hand == 0:
     print('勝ち')
-elif my_hand == 1:
-  if you_hand == 0:
+elif my_hand ==0:
+  if you_hand == 2:
     print('負け')
-  elif you_hand == 1:
+  elif you_hand == 0:
     print('あいこ')
